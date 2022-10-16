cba6a6f5d6f45ca43f7f69678917ec1e0eaed0d2
Justus Lambrecht <justuslambrecht@icloud.com>
2022-10-16 19:06:36 +0200

    image upload

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
-User.destroy_all
+require 'open-uri'
+
 ChatRoom.destroy_all
+User.destroy_all
 
 puts "Create 2 users..."
+
+url1 = File.open(Rails.root.join('db/images/test1.jpg'))
 
 natalia = User.new(
   username: "natalia",
@@ -22,6 +19,8 @@ justus = User.new(
   email: "justus@test.com",
   password: "testtest"
 )
+justus.save!
+justus.photo.attach(io: url1, filename: "p1.png", content_type: "image/png")
 justus.save!
 
 chatroom1 = ChatRoom.new(
